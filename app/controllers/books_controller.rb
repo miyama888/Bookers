5e58e0632ad16dfc13b97c3a2b92d74041dcf920
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-07-09 22:09:00 +0900

    Add

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def top
+  end
+  
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+end
