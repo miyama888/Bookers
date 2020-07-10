4223c3a45d90786c9d45417ec4f4b0b5cc84e962
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-07-10 22:25:52 +0900

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'books#top'
 
-	new '' => 'books#'
-
-	post '214143' => 'books#create
+  post 'books' => 'books#create'
 
   resources :books
 end
