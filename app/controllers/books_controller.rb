8fe019d53d1fd934204d38239ed9007a04968477
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-07-12 17:13:04 +0900

    HTML[finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def top
   end
- 
+
   def index
     @book = Book.new
     @books = Book.all
@@ -16,9 +16,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+     @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -26,15 +31,18 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path(book)
+    redirect_to books_path(book), notice: "Book was successfully destroyed."
   end
 
   private
